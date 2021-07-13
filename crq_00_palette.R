f2b21c40fedca3f308b91004140f40598a68ca3e
alanthompsonch <87364722+alanthompsonch@users.noreply.github.com>
2021-07-13 11:53:28 +0200

    Add files via upload

---
+++ crq_00_palette.R
@@ -0,0 +1,23 @@
+# defining the colour palette; add and edit colours here
+viamia_colors <- c(
+  `blue`     = "#0077BB",
+  `cyan`     = "#33BBEE",
+  `teal`     = "#009988",
+  `orange`   = "#EE7733",
+  `red`      = "#CC3311",
+  `magenta`  = "#EE3377",
+  `grey`     = "#BBBBBB",
+  `white`    = "#000000"
+)
+
+# Function to extract the colours as hex codes
+# @param ... character names of the colours -> palette_viamia("blue", "teal")
+
+palette_viamia <- function(...) {
+  cols <- c(...)
+  
+  if (is.null(cols))
+    return (viamia_colors)
+  
+  viamia_colors[cols]
+}
