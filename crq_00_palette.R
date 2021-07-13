c01e746e4914edf5e2029a8fd4b1b6fadfb43b96
alanthompsonch <87364722+alanthompsonch@users.noreply.github.com>
2021-07-13 15:58:46 +0200

    Update crq_00_palette.R

--- crq_00_palette.R
+++ crq_00_palette.R
@@ -1,14 +1,14 @@
 # defining the colour palette; add and edit colours here
-# viamia_colors <- c(
-#  `blue`     = "#0077BB",
-#  `cyan`     = "#33BBEE",
-#  `teal`     = "#009988",
-#  `orange`   = "#EE7733",
-#  `red`      = "#CC3311",
-#  `magenta`  = "#EE3377",
-#  `grey`     = "#BBBBBB",
-#  `white`    = "#000000"
-#)
+ viamia_colors <- c(
+  "blue"     = "#0077BB",
+  "cyan"     = "#33BBEE",
+  "teal"     = "#009988",
+  "orange"   = "#EE7733",
+  "red"      = "#CC3311",
+  "magenta"  = "#EE3377",
+  "grey"     = "#BBBBBB",
+  "white"    = "#000000"
+)
 
 colors_viamia <- c("#0077BB", "#33BBEE", "#009988", "#EE7733", "#CC3311", "#EE3377", "#BBBBBB", "#000000") 
 
